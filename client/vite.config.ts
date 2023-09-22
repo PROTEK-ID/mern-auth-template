d12c9281f537a0e4bfd3374d834a8b9eecf7ea16
Fitrah Nuno Syahbani <68449470+BanDroid@users.noreply.github.com>
2023-09-22 21:37:29 +0700

    feat: set base path if hosted in subroute

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "../server/dist/public",
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name].[hash].js",
-        assetFileNames: "assets/[name].[hash][extname]",
-        chunkFileNames: "assets/[name].[hash][extname]",
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASENAME,
+    build: {
+      outDir: "../server/dist/public",
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/[name].[hash].js",
+          assetFileNames: "assets/[name].[hash][extname]",
+          chunkFileNames: "assets/[name].[hash][extname]",
+        },
       },
     },
-  },
-});
+  });
+};
