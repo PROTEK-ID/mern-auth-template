a7fcb5bb9679bbb6340612bd386d3ea9e3c6641a
Fitrah Nuno Syahbani <68449470+BanDroid@users.noreply.github.com>
2023-09-22 21:41:02 +0700

    feat: set basename to follow VITE_BASENAME
    needed in case someone hosted in subroute of origins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter basename={import.meta.env.VITE_BASENAME || "/"}>
+      <></>
+    </BrowserRouter>
+  );
 }
 
 export default App;
